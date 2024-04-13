ae90e9b2d6c514ee48ee505f5db84afb58d9f76a
Hiroshi <mnakasone3@my.bcit.ca>
2024-04-12 22:37:01 -0700

    Fixed Css of cards, removed button in hero

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+
+
 export default function HeroSection() {
     return(
         <section id="heroSection" className="hero--section">
@@ -14,7 +17,6 @@ export default function HeroSection() {
                     I am passionate about creating software that is both functional and beautiful. 
                     </p>
             </div>
-            <button className="btn btn-primary">Get in Touch</button>
         </div>
         <div className="hero--section--img">
             <img src="./img/hero_img.png" alt="hero_img" />
